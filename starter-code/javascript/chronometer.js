5c1c1ca66a5bafe16c1153a342a9ee70b0941a28
angelmc32 <angel.melendez.c@gmail.com>
2019-08-03 16:14:48 -0500

    Start exercise

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,32 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      printTime();
+      //console.log(this.currentTime);
+    }, 1000);
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  twoDigitsNumber(value) {
+    if (value < 10) return `0${value}`;
+    return `${value}`;
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+    printTime();
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,80 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
-
+var btnLeft = document.getElementById("btnLeft");
+var btnRight = document.getElementById("btnRight");
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
 
 function printTime() {
-
+  let minutes = printMinutes(chronometer);
+  let seconds = printSeconds(chronometer);
+  console.log("concha " + minutes, "mierda " + seconds);
+  minDec.innerText = minutes[0];
+  minUni.innerText = minutes[1];
+  secDec.innerText = seconds[0];
+  secUni.innerText = seconds[1];
 }
 
-function printMinutes() {
-
+function printMinutes(chrono) {
+  return chrono.twoDigitsNumber(chrono.getMinutes());
 }
 
-function printSeconds() {
-
+function printSeconds(chrono) {
+  return chrono.twoDigitsNumber(chrono.getSeconds());
 }
 
-function printMilliseconds() {
+function printMilliseconds() {}
 
-}
+function printSplit() {}
 
-function printSplit() {
+function clearSplits() {}
 
-}
+function setStopBtn() {}
 
-function clearSplits() {
+function setSplitBtn() {}
 
-}
+function setStartBtn() {}
 
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
+function setResetBtn() {}
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener("click", function() {
+  let clase = btnLeft.getAttribute("class");
+  console.log(chronometer.currentTime);
 
+  if (clase === "btn start") {
+    chronometer.startClick();
+    printTime();
+    btnLeft.setAttribute("class", "btn stop");
+    btnLeft.innerText = "STOP";
+    btnRight.setAttribute("class", "btn split");
+    btnRight.innerText = "SPLIT";
+  } else {
+    chronometer.stopClick();
+    btnLeft.setAttribute("class", "btn start");
+    btnLeft.innerText = "START";
+    btnRight.setAttribute("class", "btn reset");
+    btnRight.innerText = "RESET";
+  }
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
+btnRight.addEventListener("click", function() {
+  let clase = btnRight.getAttribute("class");
 
+  if (clase === "btn reset") {
+    chronometer.resetClick();
+    if (btnLeft.innerText === "START") {
+      btnRight.setAttribute("class", "btn reset");
+      btnRight.innerText = "RESET";
+    }
+  } else {
+    if (btnLeft.innerText === "STOP") {
+      btnRight.setAttribute("class", "btn split");
+      btnRight.innerText = "SPLIT";
+    }
+  }
 });
